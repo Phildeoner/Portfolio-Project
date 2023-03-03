53fbc48960b0cfed607f8afaacc0b1afd629c7d7
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-03-03 20:19:57 +0100

    Update indentations in script.js

--- script.js
+++ script.js
@@ -26,28 +26,29 @@ function toggleMenu() {
 
 menuBtn.addEventListener('click', toggleMenu);
 
-window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};     
-      formbutton("create", {
-        action: "https://formspree.io/f/xdovzlev",
-        title: "Message Me!",
-        fields: [
-        { 
-            type: "text",  
-            name: "subject",
-            required: true,
-            placeholder: "Subject"
-          },
-          { 
-            type: "email", 
-            name: "email",
-            required: true,
-            placeholder: "Enter email address"
-          },
-          {
-            type: "textarea",
-            name: "message",
-            placeholder: "Type your message",
-          },
-          { type: "submit" }      
-        ],
-      });
+window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};
+
+formbutton("create", {
+  action: "https://formspree.io/f/xdovzlev",
+  title: "Message Me!",
+  fields: [
+    { 
+      type: "text",  
+      name: "subject",
+      required: true,
+      placeholder: "Subject"
+    },
+    { 
+      type: "email", 
+      name: "email",
+      required: true,
+      placeholder: "Enter email address"
+    },
+    {
+      type: "textarea",
+      name: "message",
+      placeholder: "Type your message",
+    },
+    { type: "submit" }      
+  ],
+});
