c7bc4b2897aea9975c62af929da47e43c3f35453
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-05-16 10:20:52 +0100

    Update script.js file

--- script.js
+++ script.js
@@ -26,6 +26,25 @@ function toggleMenu() {
 
 menuBtn.addEventListener('click', toggleMenu);
 
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    const top = window.scrollY;
+    const offset = sec.offsetTop - 150;
+    const height = sec.offsetHeight;
+    const id = sec.getAttribute('id');
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove('active');
+        document.querySelector(`header nav a[href*=${id}]`).classList.add('active');
+      });
+    }
+  });
+};
+
 window.formbutton=window.formbutton||function(){(formbutton.q=formbutton.q||[]).push(arguments)};     
       formbutton("create", {
         action: "https://formspree.io/f/xdovzlev",
